8218734a579386ab3825ff9dc9f347080a7db657
Hasya <104381534@students.swinburne.edu.my>
2025-09-23 21:04:25 +0800

    Update navigation and styles; improve About Us and Televisions pages

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
 function goToHome() {
-    window.location.href = "index.html";
+    window.location.href = "Index.html";
 }
 
-function goToTELEVISIONS() {
-    window.location.href = "televisions.html";
+function goToTelevisions() {
+    window.location.href = "Televisions.html";
 }
 
 function goToAboutUs() {
-    window.location.href = "about.html";
+    window.location.href = "Aboutus.html";
+}
+
+
+function highlightActiveNav() {
+   
+}
+
+window.onload = function() {
+    highlightActiveNav();
+    showDateTime();
+};
+
+function showDateTime() {
+    var dtElem = document.getElementById('date-time');
+    if (!dtElem) return;
+    function update() {
+        var now = new Date();
+        var year = now.getFullYear();
+        var time = now.toLocaleTimeString();
+        dtElem.textContent = ` ${time} | Year: ${year}`;
+    }
+    update();
+    setInterval(update, 1000);
 }
