78bced48b3298b9195025fb93c4ecf3e25e6cee7
Hasya <104381534@students.swinburne.edu.my>
2025-09-30 20:23:36 +0800

    modified:   about.html 	modified:   index.html 	modified:   script.js 	modified:   televisions.html

--- script.js
+++ script.js
@@ -14,17 +14,16 @@ function goToAboutUs() {
 
 window.onload = function() {
 
-    showDateTime();
+    showyear();
 };
 
-function showDateTime() {
-    var dtElem = document.getElementById('date-time');
+function showyear() {
+    var dtElem = document.getElementById('year');
     if (!dtElem) return;
     function update() {
         var now = new Date();
         var year = now.getFullYear();
-        var time = now.toLocaleTimeString();
-        dtElem.textContent = ` ${time} | Year: ${year}`;
+        dtElem.textContent = `Year: ${year}`;
     }
     update();
     setInterval(update, 1000);
